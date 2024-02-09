c665e6ed4e878402c28462006c0ae223a29c5c88
Digonil <diego.niles@hotmail.com>
2024-02-09 20:53:06 -0300

    Adicionado action para inserir motorista

--- src/main/java/com/niles/CAR/controller/DriverController.java
+++ src/main/java/com/niles/CAR/controller/DriverController.java
@@ -1,18 +1,16 @@
 package com.niles.CAR.controller;
 
-import com.niles.CAR.domain.Driver;
+import com.niles.CAR.dto.CreateDriverDto;
 import com.niles.CAR.dto.RetrieveDriverDto;
 import com.niles.CAR.services.DriverService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
-import java.util.Optional;
 
 @Controller
 @RequestMapping(value = "/drivers")
@@ -31,5 +29,13 @@ public class DriverController {
     public ResponseEntity<RetrieveDriverDto> findById(@PathVariable Long id) {
         RetrieveDriverDto driver = service.findById(id);
         return ResponseEntity.ok().body(driver);
+    }
+
+    @PostMapping
+    public ResponseEntity<RetrieveDriverDto> createDriver(@RequestBody CreateDriverDto dto) {
+        RetrieveDriverDto driver = service.createDriver(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").
+                buildAndExpand(driver.getId()).toUri();
+        return ResponseEntity.created(uri).body(driver);
     }
 }
--- src/main/java/com/niles/CAR/services/DriverService.java
+++ src/main/java/com/niles/CAR/services/DriverService.java
@@ -2,10 +2,12 @@ package com.niles.CAR.services;
 
 import com.niles.CAR.domain.Driver;
 import com.niles.CAR.domain.DriverRepository;
+import com.niles.CAR.dto.CreateDriverDto;
 import com.niles.CAR.dto.RetrieveDriverDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
@@ -18,6 +20,7 @@ public class DriverService {
     @Autowired
     private DriverRepository repository;
 
+    @Transactional(readOnly = true)
     public List<RetrieveDriverDto> findAll() {
         List<Driver> drivers = repository.findAll();
         List<RetrieveDriverDto> dtoDrivers = new ArrayList<>();
@@ -27,9 +30,19 @@ public class DriverService {
         return dtoDrivers;
     }
 
+    @Transactional(readOnly = true)
     public RetrieveDriverDto findById(Long id) {
         Optional<Driver> driverOpt = repository.findById(id);
         Driver driver = driverOpt.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
         return new RetrieveDriverDto(driver);
     }
+
+    @Transactional
+    public RetrieveDriverDto createDriver(CreateDriverDto dto) {
+        Driver driver = new Driver(dto);
+        Driver saveDriver = repository.save(driver);
+        return new RetrieveDriverDto(saveDriver);
+    }
+
+
 }
